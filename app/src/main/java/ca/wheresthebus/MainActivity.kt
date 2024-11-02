a4656354c90ace8381437c6bfc79ac8d1c227e9f
Joshua Li <jla747@sfu.ca>
2024-11-02 14:22:38 -0700

    Initial commit. Basic bottom navigation with 4 fragments.

---
+++ app/src/main/java/ca/wheresthebus/MainActivity.kt
@@ -0,0 +1,61 @@
+package ca.wheresthebus
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import ca.wheresthebus.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpNavBar()
+    }
+
+    private fun setUpNavBar() {
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_trips, R.id.navigation_nearby, R.id.navigation_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        NavigationBarView.OnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.navigation_home -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.navigation_trips -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                R.id.navigation_nearby -> {
+
+                    true
+                }
+                R.id.navigation_settings -> {
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
