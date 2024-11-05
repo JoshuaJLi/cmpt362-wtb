3198cbe47361b6f378e48256a1b7a412705d98a0
Ethan <elc10@sfu.ca>
2024-11-05 13:10:56 -0800

    added carbon copy files

---
+++ app/src/main/java/ca/wheresthebus/data/mongo_model/Route.kt
@@ -0,0 +1,20 @@
+package ca.wheresthebus.data.mongo_model
+
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.RealmList
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+// Represents routes
+class Route : RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId()
+    // route_id
+    //var id : RouteId? = null
+    var id: String = ""
+    var shortName : String = "John"
+    var longName : String = "Doe"
+    //var tripIds : RealmList<TripId> = realmListOf()
+    var tripIds: RealmList<String> = realmListOf()
+}
---
+++ app/src/main/java/ca/wheresthebus/data/mongo_model/StopTime.kt
@@ -0,0 +1,17 @@
+package ca.wheresthebus.data.mongo_model
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+// Represents stop_times and trips
+class StopTime : RealmObject {
+    @PrimaryKey var _id: ObjectId = ObjectId()
+    //var arrivalTime: LocalDateTime? = null
+    var arrivalTime: Long = 0L
+    //stop_id
+    var id : String = ""
+    var routeId: String = ""
+    var serviceId : String = ""
+    var tripId : String = ""
+}
