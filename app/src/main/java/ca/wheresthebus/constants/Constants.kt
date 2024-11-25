5abe93357e7eb1b8074e46e578c38c1d02f86a14
yka92 <yka92@sfu.ca>
2024-11-24 22:16:12 -0800

    delete constants

--- app/src/main/java/ca/wheresthebus/constants/Constants.kt
+++
@@ -1,9 +0,0 @@
-package ca.wheresthebus.constants
-
-object Constants {
-
-    // -- NearbyService
-    const val ACTION_START_SERVICE = "ACTION_START_SERVICE";
-    const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE";
-
-}
