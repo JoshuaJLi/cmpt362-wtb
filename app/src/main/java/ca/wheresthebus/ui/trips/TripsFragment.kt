487238356c8f2a9440153c420e6a7900849dabdd
Joshua Li <jla747@sfu.ca>
2024-11-24 22:28:21 -0800

    Add labels for different types of trips.

--- app/src/main/java/ca/wheresthebus/ui/trips/TripsFragment.kt
+++ app/src/main/java/ca/wheresthebus/ui/trips/TripsFragment.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Visibility
 import ca.wheresthebus.MainDBViewModel
 import ca.wheresthebus.adapter.TripAdapter
 import ca.wheresthebus.databinding.FragmentTripsBinding
@@ -20,8 +20,13 @@ class TripsFragment : Fragment() {
     private lateinit var tripsViewModel : TripsViewModel
     private lateinit var mainDBViewModel: MainDBViewModel
 
-    private lateinit var tripAdapter: TripAdapter
-    private lateinit var tripsView : RecyclerView
+    private lateinit var activeTripAdapter: TripAdapter
+    private lateinit var upcomingTripAdapter: TripAdapter
+    private lateinit var inactiveTripAdapter: TripAdapter
+
+    private lateinit var activeTripsView : RecyclerView
+    private lateinit var upcomingTripsView : RecyclerView
+    private lateinit var inactiveTripsView : RecyclerView
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -49,20 +54,65 @@ class TripsFragment : Fragment() {
 
         val trips = mainDBViewModel.getTrips()
             .sortedBy { it.getClosestTime(currentTime) }
-            .partition { it.isActive(currentTime) }
-            .let { (active, inactive) -> active + inactive }
+            .groupBy { trip ->
+                when {
+                    trip.isActive(currentTime) -> TripType.ACTIVE
+                    trip.isToday(currentTime) -> TripType.TODAY
+                    else -> TripType.INACTIVE
+                }
+            }
 
-        tripAdapter = TripAdapter(trips)
-        tripsView = binding.recyclerTrips
+        activeTripsView = binding.recyclerActiveTrips
+        inactiveTripsView = binding.recyclerInactiveTrips
+        upcomingTripsView = binding.recyclerUpcomingTrips
 
-        tripsView.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = tripAdapter
+        trips[TripType.ACTIVE].orEmpty().let {
+            if (it.isEmpty()) {
+                binding.labelActive.visibility = View.GONE
+            }
+            activeTripAdapter = TripAdapter(it)
+
+            activeTripsView.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = activeTripAdapter
+            }
+        }
+
+        trips[TripType.TODAY].orEmpty().let {
+            if (it.isEmpty()) {
+                binding.labelUpcomingTrips.visibility = View.GONE
+            }
+            upcomingTripAdapter = TripAdapter(it)
+
+            upcomingTripsView.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = upcomingTripAdapter
+            }
+        }
+
+        trips[TripType.INACTIVE].orEmpty().let {
+            if (it.isEmpty()) {
+                binding.labelAllTrips.visibility = View.GONE
+            }
+            inactiveTripAdapter = TripAdapter(it)
+
+            inactiveTripsView.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = inactiveTripAdapter
+            }
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        object TripType {
+            const val ACTIVE = "active"
+            const val INACTIVE = "inactive"
+            const val TODAY = "today"
+        }
     }
 }
