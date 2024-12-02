470e76f4f0e4e71cd663255e8df53bb889c3772e
Johnny <52947117+Toastjpg@users.noreply.github.com>
2024-12-01 23:32:49 -0800

    improved search feature to be less jank

--- app/src/main/java/ca/wheresthebus/MainDBViewModel.kt
+++ app/src/main/java/ca/wheresthebus/MainDBViewModel.kt
@@ -137,18 +137,15 @@ class MainDBViewModel : ViewModel() {
     // todo: can improve search by sorting by closest location
     fun searchForStop(input: String): List<BusStop> {
         // search the name parts by any matching string tokens
-        val words = input.split(" ").filter { it.isNotEmpty() }
-        val fuzzyQuery = List(words.size) { i ->
-            "(name CONTAINS[c] $${i + 1} OR ANY mongoRoutes.longName CONTAINS[c] $${i + 1})"
+        val tokens = input.split(" ").filter { it.isNotEmpty() }
+        val fuzzyQuery = List(tokens.size) { i ->
+            "(code == $${i} OR ANY mongoRoutes.shortName == $${i} OR name CONTAINS[c] $${i})"
         }.joinToString(" AND ")
 
-        // Strict search by code or route shortname
-        val query = "code == $0 OR ANY mongoRoutes.shortName == $0 OR ($fuzzyQuery)"
         val result = realm.query<MongoBusStop>(
-            query,
-            input,
-            *words.toTypedArray() // spread operator to pass string tokens as query params
-        ).find().take(10)
+            fuzzyQuery,
+            *tokens.toTypedArray() // spread operator to pass string tokens as query params
+        ).find().takeWhile { true }
 
         // Return result as a List<BusStops> instead of List<MongoBusStops>
         return result.map { modelFactory.toBusStop(it) }
