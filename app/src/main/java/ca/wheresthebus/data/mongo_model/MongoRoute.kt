3aaa72d0b02c7058d0e388a90a9a1c5cd3c4936d
Johnny Mai <52947117+toastjpg@users.noreply.github.com>
2024-12-03 07:36:00 -0800

    Update MongoRoute.kt

--- app/src/main/java/ca/wheresthebus/data/mongo_model/MongoRoute.kt
+++ app/src/main/java/ca/wheresthebus/data/mongo_model/MongoRoute.kt
@@ -4,9 +4,7 @@ import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.Index
 import io.realm.kotlin.types.annotations.PrimaryKey
 
-// Represents routes
 class MongoRoute : RealmObject {
-    // route_id
     @PrimaryKey var id: String = ""
     @Index var shortName : String = ""
     @Index var longName : String = ""
