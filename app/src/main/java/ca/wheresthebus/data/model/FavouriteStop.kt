2cef3369efa1b29bd1ab01b2e202e5ad8d27bd9e
Johnny Mai <52947117+toastjpg@users.noreply.github.com>
2024-12-03 07:33:39 -0800

    Update FavouriteStop.kt

--- app/src/main/java/ca/wheresthebus/data/model/FavouriteStop.kt
+++ app/src/main/java/ca/wheresthebus/data/model/FavouriteStop.kt
@@ -2,6 +2,7 @@ package ca.wheresthebus.data.model
 
 import org.mongodb.kbson.ObjectId
 
+// Represents a specific bus stop saved by the user
 data class FavouriteStop(
     val _id: ObjectId = ObjectId(),
     val nickname: String,
